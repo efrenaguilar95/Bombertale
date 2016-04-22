d8b1888e981cddbb5b70b638cb9b91e0f6057662
Roldan Gammad <rgammad@uci.edu>
2016-04-22 15:55:24 -0700

    fixed menu to lobby screen. fixed server load on awake bug
    Efren Aguilar, Alex Deuling, Roldan Gammad

--- Bombertale/Assets/Scripts/ServerManager.cs
+++ Bombertale/Assets/Scripts/ServerManager.cs
@@ -12,6 +12,8 @@ public class ServerManager : MonoBehaviour
     private int serverCount;
     private string unityPassword = "ICS168";
 
+    List<GameObject> toDelete = new List<GameObject>();
+
     struct Server
     {
         public string serverName;
@@ -25,26 +27,51 @@ public class ServerManager : MonoBehaviour
             serverName = serverString[0];
             IP = serverString[1];
             port = int.Parse(serverString[2]);
-            security = (int.Parse(serverString[3])==1? true : false);
+            security = (int.Parse(serverString[3]) == 1 ? true : false);
             password = serverString[4];
             playerCount = int.Parse(serverString[5]);
         }
+    }
+
+    void Start()
+    {
+        Refresh();
     }
 
     public void Refresh()
     {
         splitString(GetServers());
         servers.Clear();
+        //Debug.Log(transform.parent.Find("Scroll View").Find("Viewport").Find("Content").childCount);
+        //if (transform.parent.Find("Scroll View").Find("Viewport").Find("Content").childCount == null)
+        //{
+        //    return;
+        //}
+        ClearServers();
         for (int i = 0; i < serverCount; i++)
         {
             string[] temp;
             temp = serverList[i].Split('&');
             servers.Add(new Server(temp));
         }
-        Debug.Log(servers[0].serverName);
-        //GameObject tempObject = null;
-        //tempObject = Instantiate(serverInfo, this.transform.position, Quaternion.identity) as GameObject;
-        //tempObject.transform.SetParent(transform.parent.FindChild("Canvas").transform);
+        //Debug.Log(servers[0].serverName);
+        GameObject tempObject = null;
+
+        foreach (Server temp in servers)
+        {
+            tempObject = Instantiate(serverInfo, this.transform.position, Quaternion.identity) as GameObject;
+            tempObject.transform.SetParent(transform.parent.Find("Scroll View/Viewport/Content"));
+            tempObject.transform.localScale = Vector3.one;
+            tempObject.transform.Find("LobbyName").GetComponent<UnityEngine.UI.Text>().text = temp.serverName;
+            tempObject.transform.Find("Players").GetComponent<UnityEngine.UI.Text>().text = temp.playerCount + "/4";
+            if (temp.security)
+            {
+                tempObject.transform.Find("Security").GetComponent<UnityEngine.UI.Image>().sprite = Resources.Load<Sprite>("lock");
+            }
+
+            toDelete.Add(tempObject);
+
+        }
     }
 
     private void splitString(string message)
@@ -53,6 +80,13 @@ public class ServerManager : MonoBehaviour
         temp = message.Substring(6, message.Length - 6);
         serverList = temp.Split('#');
         serverCount = serverList.Length;
+    }
+
+    private void ClearServers()
+    {
+        for (int i = 0; i < toDelete.Count; i++)
+            Destroy(toDelete[i]);
+        toDelete.Clear();
     }
 
     public string GetServers()
