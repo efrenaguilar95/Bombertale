db116cef312b2c91cee6c7c5459f965195500239
apedestrian <adeuling@uci.edu>
2016-04-26 15:42:51 -0700

    added login scene

---
+++ Bombertale/Assets/Scripts/AccountManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccountManager : MonoBehaviour {
+
+	private string unityPassword = "ICS168";
+	//void Start()
+	//{
+	//	//DeleteServer ("alex");
+	//	//CreateServer("alex2", "1.1.1.1", 100, true, "sonic", 4);
+	//	//GetServers();
+	//	//JoinServer("alex");
+	//	//UpdatePlayers("alex", 2);
+	//	//CreateAccount("alex", "sonic");
+	//	//Login("alex", "sonic");
+
+	//}
+
+	public string CreateAccount(string username, string password)
+	{
+		string url = "http://apedestrian.com/bombertale/CreateAccount.php?unityPassword=" + unityPassword +  "&clientUsername=" + username + "&clientPassword=" + password;
+		return GetText (url);
+	}
+
+	public string Login(string username, string password)
+	{
+		string url = "http://apedestrian.com/bombertale/Login.php?unityPassword=" + unityPassword +  "&clientUsername=" + username + "&clientPassword=" + password;
+		return GetText (url);
+	}
+
+	private string GetText(string url)
+	{
+		WWW www = new WWW(url.Replace(" ", "%20"));
+		StartCoroutine(WaitForRequest(www));
+		while(!www.isDone)
+		{
+			//...
+		}
+		return www.text;
+	}
+
+	private IEnumerator WaitForRequest(WWW www)
+	{
+		yield return www;  
+	}
+}
--- Bombertale/Assets/Scripts/UIManager.cs
+++ Bombertale/Assets/Scripts/UIManager.cs
@@ -5,17 +5,81 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
 
+	private string unityPassword = "ICS168";
+
     public void PressLocalGame()
     {
         SceneManager.LoadScene("Main");
     }
     public void PressJoinLobby()
     {
-        SceneManager.LoadScene("Lobby");
+        SceneManager.LoadScene("Login");
     } 
     public void BackToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
+
+	public void Login()
+	{
+		string username = GameObject.Find("Canvas/Username").GetComponent<UnityEngine.UI.InputField>().text;
+		string password = GameObject.Find("Canvas/Password").GetComponent<UnityEngine.UI.InputField>().text;
+
+		string returnCode = Login (username, password);
+
+		returnCode = returnCode.Substring (0, 4);
+		Debug.Log (returnCode);
+
+		if (returnCode == "BC04")
+		{
+			//login success
+			//WE NEED TO SET A GLOBAL USERNAME HERE
+			GameObject.Find("Canvas/WrongLoginText").GetComponent<UnityEngine.UI.Text>().text = "";
+			SceneManager.LoadScene ("Lobby");
+		}
+		else if(returnCode == "BC02" || returnCode == "BC03")
+		{
+			//wrong login information
+			GameObject.Find("Canvas/WrongLoginText").GetComponent<UnityEngine.UI.Text>().text = "Wrong Login Information";
+		}
+		else
+		{
+			//could not reach server
+			GameObject.Find("Canvas/WrongLoginText").GetComponent<UnityEngine.UI.Text>().text = "Couldn't Reach Server";
+		}
+	}
+
+	public void CreateAccount()
+	{
+		//SceneManager.LoadScene("MainMenu");
+	}
+
+	public void ForgotLogin()
+	{
+		//SceneManager.LoadScene("MainMenu");
+	}
+
+
+	public string Login(string username, string password)
+	{
+		string url = "http://apedestrian.com/bombertale/Login.php?unityPassword=" + unityPassword +  "&clientUsername=" + username + "&clientPassword=" + password;
+		return GetText (url);
+	}
+
+	private string GetText(string url)
+	{
+		WWW www = new WWW(url.Replace(" ", "%20"));
+		StartCoroutine(WaitForRequest(www));
+		while(!www.isDone)
+		{
+			//...
+		}
+		return www.text;
+	}
+
+	private IEnumerator WaitForRequest(WWW www)
+	{
+		yield return www;  
+	}
 
 }
