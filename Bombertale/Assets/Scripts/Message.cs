7095f7ec80aa02971136f33a544b3127ea338953
eaho1 <eaho1@uci.edu>
2016-04-29 14:27:39 -0700

    Abstracted message stuff
    Efren Aguilar, Alex Deuling, Roldan Gammad, Edwin Ho

--- Bombertale/Assets/Scripts/Message.cs
+++ Bombertale/Assets/Scripts/Message.cs
@@ -1,7 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+public enum MessageType
+{
+    None,
+    LobbyUpdate
+}
 
 [System.Serializable]
 public class Message{
+    public MessageType type;
+    public string subJson;
 
+
+    public Message(MessageType type = MessageType.None, object data = null)
+    {
+        this.type = type;
+        this.subJson = JsonUtility.ToJson(data);
+    }
+
+    public object GetData()
+    {
+        switch (this.type)
+        {
+            case MessageType.LobbyUpdate:
+                return JsonUtility.FromJson<int>(subJson);
+            default:
+                return null;
+        }
+    }
 }
