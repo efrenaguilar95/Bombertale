7594adb6bcb21fcb9d9a62bb6c8c0f8d3b9fda2e
efrenaguilar95 <efrenaguilar95@hotmail.com>
2016-05-08 10:42:18 -0700

    Fixed Snowdin Music Bug
    SnowdinMusic Manager was making duplicates of itself, leading to
    duplicate tracks playing. Added a singleton class to fix this
    duplication issue.

---
+++ Bombertale/Assets/Scripts/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singleton<Instance> : MonoBehaviour where Instance : Singleton <Instance> {
+    public static Instance instance = null;
+    public bool isPersistant;
+
+
+    public virtual void Awake()
+    {
+        if(isPersistant)
+        {
+            if (instance == null)
+            {
+                instance = this as Instance;
+            }
+
+            else
+            {
+                DestroyObject(gameObject);
+            }
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            instance = this as Instance;
+        }
+    }
+}
--- Bombertale/Assets/Scripts/SnowedinMusicManager.cs
+++ Bombertale/Assets/Scripts/SnowedinMusicManager.cs
@@ -2,18 +2,14 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class SnowedinMusicManager : MonoBehaviour {
-    void Awake()
-    {
-        DontDestroyOnLoad(this.gameObject);
-    }
-
+public class SnowedinMusicManager : Singleton<SnowedinMusicManager> {
     void OnLevelWasLoaded(int level)
     {        
         string loadedScene = SceneManager.GetActiveScene().name;
         if (loadedScene == "MainMenu" || loadedScene == "ServerGame" || loadedScene == "ClientGame")
         {
             Destroy(this.gameObject);
+            instance = null;
         }
     }
 }
